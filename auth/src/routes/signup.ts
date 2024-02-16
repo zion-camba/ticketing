cf1b27b0b2003837785e40523c6420f326460d5b
Zion <macbookpro@Zion.local>
2024-02-16 11:20:00 +0800

    created user model and documnet

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -21,12 +21,19 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
 
-    throw new DatabaseConnectionError();
     const { email, password } = req.body;
 
-    console.log("creating a user...");
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      console.log("Email in use");
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
