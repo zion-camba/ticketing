d2a265f805cda05bad0f155cd4d720b66daf58d1
Zion <macbookpro@Zion.local>
2024-02-19 23:19:30 +0800

    created sign up router

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-
+import { BadRequestError } from "../errors/bad-requests-error";
 const router = express.Router();
 
 router.post(
@@ -26,8 +26,7 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      console.log("Email in use");
-      return res.send({});
+      throw new BadRequestError("Email in use");
     }
 
     const user = User.build({ email, password });
