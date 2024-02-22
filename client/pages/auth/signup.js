b60f3469d24f8ff14fcceaa0de5e41002a534e44
Zion <macbookpro@Zion.local>
2024-02-22 11:35:05 +0800

    initialized client

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary">Sign up</button>
+    </form>
+  );
+};
+
+export default Signup;
